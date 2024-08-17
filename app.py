320bbaa8d61feb19d5468abc3d31ae2c3e0ce034
Nestum447 <ncarpio935@gmail.com>
2024-08-17 10:20:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ with open('modelo_precio_casas.pkl', 'rb') as file:
 st.title('Carpio Machine Learning Predicción de Precio de Casas El Salvador ')
 
 # Entradas de usuario
-tamaño = st.number_input('Tamaño de la casa (en metros cuadrados):', min_value=500, max_value=10000, value=1500)
-habitaciones = st.number_input('Número de habitaciones:', min_value=1, max_value=10, value=3)
+tamaño = st.number_input('Tamaño de la casa (en metros cuadrados):', min_value=40, max_value=300, value=50)
+habitaciones = st.number_input('Número de habitaciones:', min_value=1, max_value=10, value=2)
 edad = st.number_input('Edad de la casa (en años):', min_value=0, max_value=100, value=10)
 
 # Hacer la predicción cuando se presione el botón
